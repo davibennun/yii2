8959a0a9162c8edb0fe3f78f0a2daca7ecb24c6c
Davi Nunes <davi@andradenunes.org>
2016-05-09 11:23:49 -0400

    begin acceptance testing basic customer feature(add,query)

---
+++ tests/_support/Step/Acceptance/CRMUserSteps.php
@@ -0,0 +1,42 @@
+<?php
+namespace Step\Acceptance;
+
+class CRMUserSteps extends \AcceptanceTester
+{
+	public function amInQueryCustomerUI()
+	{
+		$I = $this;
+		$I->amOnPage('/customers/query');
+	}
+
+	public function fillInPhoneFieldWithDataFrom($customer_data)
+	{
+		$I = $this;
+		$I->fillField('PhoneRecord[number]', $customer_data['PhoneRecord[number]']);
+	}
+
+	public function clickSearchButton()
+	{
+		$I = $this;
+		$I->click('Search');
+	}
+
+	public function seeIAmInListCustomersUi()
+	{
+		$I = $this;
+		$I->seeCurrentUrlMatches('/customers/');
+	}
+
+	public function seeCustomerInList($customer_data)
+	{
+		$I = $this;
+		$I->see($customer_data['CustomerRecord[name]'], '#search_results');
+	}
+
+	public function dontSeeCustomerInList($customer_data)
+	{
+		$I = $this;
+		$I->dontSee($customer_data['CustomerRecord[name]'], '#search_results');	
+	}
+
+}
